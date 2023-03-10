ec0f1232bb17884b951de4356be2a4b9508bbb3c
Paul Ashby <paul@primitive.co>
2023-03-10 09:49:54 +0000

    Update nav colours and hover underline offset

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,8 +7,8 @@ function Nav({ scrollToTop }) {
       <nav ref={scrollToTop} id="nav" className="relative w-full flex flex-wrap items-center justify-between px-2 py-3 bg-[#025] z-10">
         <div className="container px-3 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between md:w-auto md:static md:block md:justify-start">
-            <a className="text-3xl md:text-2xl font-bold inline-block mr-3 py-2 whitespace-nowrap font-itim text-[#add8e6] lowercase" href="/">To-Go-List</a>
-            <button className="text- cursor-pointer px-3 py-1 block md:hidden outline-none focus:outline-none text-4xl text-[#add8e6] hover:text-yellow-300" type="button" onClick={() => setNavbarOpen(!navbarOpen)}> ≡ </button>
+            <a className="text-3xl md:text-2xl font-bold inline-block mr-3 py-2 whitespace-nowrap font-itim text-blue-200 lowercase" href="/">To-Go-List</a>
+            <button className="text- cursor-pointer px-3 py-1 block md:hidden outline-none focus:outline-none text-4xl text-blue-200 hover:text-[#f5a139]" type="button" onClick={() => setNavbarOpen(!navbarOpen)}> ≡ </button>
           </div>
           <div
             className={
@@ -18,16 +18,16 @@ function Nav({ scrollToTop }) {
           >
             <ul className="flex flex-col md:flex-row list-none md:ml-auto">
               <li className="nav-item">
-                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#schedule">Schedule</a>
+                <a className=" hover:text-[#f5a139] px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-blue-200 hover:underline decoration-wavy underline-offset-8 decoration-3" href="#schedule">Schedule</a>
               </li>
               <li className="nav-item">
-                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#currency">Currency</a>
+                <a className=" hover:text-[#f5a139] px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-blue-200 hover:underline decoration-wavy underline-offset-8 decoration-3" href="#currency">Currency</a>
               </li>
               <li className="nav-item">
-                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#weather">Weather</a>
+                <a className=" hover:text-[#f5a139] px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-blue-200 hover:underline decoration-wavy underline-offset-8 decoration-3" href="#weather">Weather</a>
               </li>
               <li className="nav-item">
-                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#contact">Contact</a>
+                <a className=" hover:text-[#f5a139] px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-blue-200 hover:underline decoration-wavy underline-offset-8 decoration-3" href="#contact">Contact</a>
               </li>
             </ul>
           </div>

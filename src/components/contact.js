e56c4776ac3218cca986ce5bd11bfb6b659b22cb
Paul Ashby <paul@primitive.co>
2023-03-13 15:17:15 +0000

    Configure form for react

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,7 +9,8 @@ function Contact() {
                     <svg className="h-5 w-5 pb-1 fill-slate-300" viewBox="0 0 31.982 31.982">	<path d="M3.952,23.15L0,31.955l8.767-3.992l0.018,0.019L3.938,23.13L3.952,23.15z M4.602,22.463L24.634,2.432l4.849,4.848 L9.45,27.312L4.602,22.463z M30.883,0.941c-2.104-1.963-4.488-0.156-4.488-0.156l4.851,4.843 C31.244,5.627,33.124,3.375,30.883,0.941z" /></svg>
                 </span>
             </span>
-            <form name="contact" method="post" data-netlify="true">
+            <form name="contact" method="post">
+                <input type="hidden" name="form-name" value="contact" />
                 <p className="m-4">
                     <label className="mr-4" htmlFor="name">Name:</label>
                     <input type="text" id="name" name="name" required placeholder="Enter your name" />
